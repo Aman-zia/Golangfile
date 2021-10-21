693bf6d901d6a46e40e040bbac74ed391d539fc9
Aman Zia <81854518+Aman-zia@users.noreply.github.com>
2021-10-21 12:58:58 +0530

    Add files via upload

---
+++ map.go
@@ -0,0 +1,12 @@
+// var name = map[keytype]valuetype{"key":"value","key":"value","key":"value"}
+// syntax for writing map
+
+package main
+
+import "fmt"
+
+var student_id = map[string]int{"Aman": 1, "Raj": 2, "Sam": 3, "tim": 4, "ana": 5, "jay": 6}
+
+func main() {
+	fmt.Println(student_id)
+}
