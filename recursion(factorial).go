8532d87d6ce6946f4a99604fb334dec689ac926e
Aman Zia <81854518+Aman-zia@users.noreply.github.com>
2021-10-28 17:14:26 +0530

    Add files via upload

---
+++ recursion(factorial).go
@@ -0,0 +1,20 @@
+// when a function calling itself again and again then it is known as recursion
+
+package main
+
+import (
+	"fmt"
+)
+
+func factorial(i int) int {
+	if i <= 1 {
+		return 1
+	}
+	return i * factorial(i-1)
+}
+
+func main() {
+	var i int = 5
+	fmt.Printf("the factorial is %d\n", factorial(i))
+
+}
