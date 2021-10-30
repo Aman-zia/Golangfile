5d31127129485135d41608edb4b8573c95b6e775
Aman Zia <81854518+Aman-zia@users.noreply.github.com>
2021-10-30 16:39:03 +0530

    Add files via upload

---
+++ variadic_func.go
@@ -0,0 +1,14 @@
+//in this variadic func we can pass unlimited numbers of parameters
+package main
+
+import (
+	"fmt"
+)
+
+func variadic(params ...int) {
+	fmt.Printf("%T\n %v\n", params, params)
+}
+func main() {
+
+	variadic(1, 2, 3, 4, 5, 6, 7, 8, 9)
+}
