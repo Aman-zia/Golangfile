e894431df3e313c20543a71da4a9cdc170023464
Aman Zia <81854518+Aman-zia@users.noreply.github.com>
2021-12-02 13:13:34 +0530

    Add files via upload
    first and simple program by using gin

--- gin-gonic/gin.go
+++ gin-gonic/gin.go
@@ -1,1 +1,16 @@
-gin gonic
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", displayString)
+	r.Run(":1313")
+}
+func displayString(c *gin.Context) {
+	c.String(http.StatusOK, "Hello world")
+}
